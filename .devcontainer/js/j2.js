991887f8fbd63d5dab20068c0010ce2681d41645
patelmanav15 <161555960+patelmanav15@users.noreply.github.com>
2024-02-29 04:59:32 +0000

    conversion

---
+++ .devcontainer/js/j2.js
@@ -0,0 +1,12 @@
+// conversion 
+let isLoggedIn = "manav"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "manav" => true
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(typeof stringNumber);
