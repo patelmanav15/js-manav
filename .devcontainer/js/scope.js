c7f61d124cf569bbd1dce71785f180de76a47fab
patelmanav15 <161555960+patelmanav15@users.noreply.github.com>
2024-03-04 08:57:32 +0000

    scope

---
+++ .devcontainer/js/scope.js
@@ -0,0 +1,34 @@
+function one(){
+    const username = "manav"
+
+    function two(){
+        const website = "yt"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if (true){
+    const username= "manav"
+    if (username == "manav"){
+        const website = "youtub"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// --------- interesting------
+
+function addone(num){
+    return num + 1
+}
+addone(5)
+
+
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(5)
